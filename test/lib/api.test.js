96f616c9e5d62a963450d093e4ba41fbad114897
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-11-21 21:45:37 +0000

    tidied up setupPlayer

--- test/lib/api.test.js
+++ test/lib/api.test.js
@@ -29,7 +29,8 @@ describe('api', function () {
     });
 
     db = {
-      init: sinon.stub()
+      init: sinon.stub(),
+      setupPlayer: sinon.stub()
     };
     dbThen = promiseStub(db, 'init');
     socket = {
@@ -38,6 +39,7 @@ describe('api', function () {
       on: sinon.stub()
     };
 
+    promiseStub(db, 'setupPlayer');
     mockery.registerMock('./db', db);
 
     API = require('../../lib/api');
@@ -48,41 +50,71 @@ describe('api', function () {
     mockery.disable();
   });
 
-  it('should construct a new API', function () {
-    expect(api.dbMock).to.be.a(Function);
-    expect(api.socket).to.be(socket);
+  describe('setup', function () {
+    it('should construct a new API', function () {
+      expect(api.dbMock).to.be.a(Function);
+      expect(api.socket).to.be(socket);
+    });
+
+    it('should connect to the db', function () {
+      api.init();
+      expect(socket.emit.calledWith('info', 'Let the battle commence!')).to.be.ok();
+      expect(socket.emit.calledWith('info', 'Connecting to DB')).to.be.ok();
+      expect(dbThen.called).to.be.ok();
+      expect(dbThen.alwaysCalledWithMatch(Function, Function)).to.be.ok();
+    });
+
+    it('should handle errors', function () {
+      var err = 'foo';
+
+      api.handleDBError();
+      expect(socket.emit.calledWith('error', 'Failed to connect to DB')).to.be.ok();
+      expect(socket.close.called).to.be.ok();
+
+      api.handleError(err);
+      expect(socket.emit.calledWith('error', err)).to.be.ok();
+    });
+
+    it('should handle db success', function () {
+      sinon.stub(api, 'addPlayerListeners');
+
+      api.handleDBSuccess();
+      expect(socket.emit.calledWith('info', 'Successfully connected to DB')).to.be.ok();
+      expect(api.addPlayerListeners.called).to.be.ok();
+    });
   });
 
-  it('should connect to the db', function () {
-    api.init();
-    expect(socket.emit.calledWith('info', 'Let the battle commence!')).to.be.ok();
-    expect(socket.emit.calledWith('info', 'Connecting to DB')).to.be.ok();
-    expect(dbThen.called).to.be.ok();
-    expect(dbThen.alwaysCalledWithMatch(Function, Function)).to.be.ok();
-  });
+  describe('player', function () {
+    var data = function (playerName, playerId) {
+      return {
+        playerName: playerName,
+        playerId: playerId
+      };
+    };
 
-  it('should handle errors', function () {
-    var err = 'foo';
+    it('should listen for new players', function () {
+      api.addPlayerListeners();
+      expect(socket.emit.calledWith('info', sinon.match('Please tell me who you are'))).to.be.ok();
+      expect(socket.on.calledWith('setupPlayer', sinon.match.func)).to.be.ok();
+    });
 
-    api.handleDBError();
-    expect(socket.emit.calledWith('error', 'Failed to connect to DB')).to.be.ok();
-    expect(socket.close.called).to.be.ok();
+    it('should throw an error if a player is already defined', function () {
+      api.player = {};
+      api.setupPlayer(data());
+      expect(socket.emit.calledWith('error', 'Player is already connected')).to.be.ok();
+    });
 
-    api.handleError(err);
-    expect(socket.emit.calledWith('error', err)).to.be.ok();
-  });
+    it('should convert data values to a string', function () {
+      api.setupPlayer(data(1, 1));
+      expect(db.setupPlayer.calledWithMatch('1', '1')).to.be.ok();
+    });
 
-  it('should handle db success', function () {
-    sinon.stub(api, 'addPlayerListeners');
+    it('should require a playerName that is <= 25 chars', function () {
+      api.setupPlayer(data());
+      expect(socket.emit.calledWith('error', '`playerName` is invalid')).to.be.ok();
 
-    api.handleDBSuccess();
-    expect(socket.emit.calledWith('info', 'Successfully connected to DB')).to.be.ok();
-    expect(api.addPlayerListeners.called).to.be.ok();
-  });
-
-  it('should listen for new players', function () {
-    api.addPlayerListeners();
-    expect(socket.emit.calledWith('info', sinon.match('Please tell me who you are'))).to.be.ok();
-    expect(socket.on.calledWith('setupPlayer', sinon.match.func)).to.be.ok();
+      api.setupPlayer(data(new Array(27).join('a')));
+      expect(socket.emit.calledWith('error', '`playerName` is invalid')).to.be.ok();
+    });
   });
 });
